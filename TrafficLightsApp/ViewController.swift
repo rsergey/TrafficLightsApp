c9533f538619f76297b2a0fa3ee3eda00b3ce29f
Sergey <rumyantsev.serge@yandex.ru>
2021-07-07 19:12:20 +0300

    creating interface elements and programming logic

--- TrafficLightsApp/ViewController.swift
+++ TrafficLightsApp/ViewController.swift
@@ -8,12 +8,48 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var redLightVeiw: UIView!
+    @IBOutlet weak var yellowLightView: UIView!
+    @IBOutlet weak var greenLightView: UIView!
+    @IBOutlet weak var startButton: UIButton!
+    
+    var trafficLightFlag = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        redLightVeiw.layer.cornerRadius = redLightVeiw.frame.height / 2
+        redLightVeiw.alpha = 0.3
+        
+        yellowLightView.layer.cornerRadius = yellowLightView.frame.height / 2
+        yellowLightView.alpha = 0.3
+        
+        greenLightView.layer.cornerRadius = greenLightView.frame.height / 2
+        greenLightView.alpha = 0.3
+        
+        startButton.setTitle("Start", for: .normal)
+        startButton.layer.cornerRadius = 10
     }
 
-
+    @IBAction func nextButtonPressed() {
+        switch trafficLightFlag {
+        case 1:
+            yellowLightView.alpha = 1
+            redLightVeiw.alpha = 0.3
+            trafficLightFlag = 2
+        case 2:
+            greenLightView.alpha = 1
+            yellowLightView.alpha = 0.3
+            trafficLightFlag = 3
+        case 3:
+            redLightVeiw.alpha = 1
+            greenLightView.alpha = 0.3
+            trafficLightFlag = 1
+        default:
+            redLightVeiw.alpha = 1
+            startButton.setTitle("Next", for: .normal)
+            trafficLightFlag = 1
+        }
+    }
+    
 }
 
